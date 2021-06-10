dbd32477dbdd9585ebb4f282ca668b124c1b0053
xknk <2438756801@qq.com>
2021-06-10 10:03:17 +0800

    删除无用信息

--- src/views/Config/index.vue
+++ src/views/Config/index.vue
@@ -1,3 +1,9 @@
+<!--
+ * @Author: Robin LEI
+ * @Date: 2021-05-31 15:28:53
+ * @LastEditTime: 2021-06-10 09:49:46
+ * @FilePath: \Assembly\src\views\Config\index.vue
+-->
 <template>
     3333
-</template>git remote add origin
+</template>
